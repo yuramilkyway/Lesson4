c9d204e003e1313e55d3334843b098c6f7a121ae
YuraVolkov <yuramilkyway@gmail.com>
2020-10-08 01:02:48 +0300

    Добавление метода keySet()

--- src/ru/innopolis/main/Main.java
+++ src/ru/innopolis/main/Main.java
@@ -1,21 +1,24 @@
 package ru.innopolis.main;
 
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
 
-        HashDemoMap hashDemoMap = new HashDemoMap(10);
-        hashDemoMap.put("1", "cat");
-        hashDemoMap.put(hashDemoMap, "dog");
-        hashDemoMap.put(999999999, "yyy");
+        HashDemoMap<Integer, String> hashDemoMap = new HashDemoMap(10);
+        hashDemoMap.put(1, "cat");
+        hashDemoMap.put(24, "dog");
+        hashDemoMap.put(9, "yyy");
         hashDemoMap.put(5, "gggg");
 
-        System.out.println(hashDemoMap.get("1"));
-        System.out.println(hashDemoMap.get(hashDemoMap));
+        System.out.println(hashDemoMap.containsValue("do"));
+        System.out.println("=========================");
+        Set<Integer> set = hashDemoMap.keySet();
+        for (Integer s : set) {
+            System.out.println(s);
+        }
+        System.out.println("=========================");
 
-        hashDemoMap.put("1", "bob");
-        System.out.println(hashDemoMap.get("1"));
-        hashDemoMap.remove("1");
-        System.out.println(hashDemoMap.get("1"));
-
+        System.out.println(hashDemoMap.get(9));
     }
 }
